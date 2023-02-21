90518061c9f754796fc9e98a2fd7f27ed086f271
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-02-21 13:10:30 +0100

    Cleanup

--- src/xpdf-python/pdftostring.cc
+++ src/xpdf-python/pdftostring.cc
@@ -29,20 +29,11 @@
 
 #include "pdftostring.h"
 
-// static GBool clipText = gFalse;
-// static GBool discardDiag = gTrue;
-// static GBool discardRotatedText = gTrue;
-// static GBool noPageBreaks = gFalse;
-// static GBool insertBOM = gFalse;
-// static char ownerPassword[33] = "\001";
-// static char userPassword[33] = "\001";
-// static GBool verbose = gFalse;
-// static GBool quiet = gFalse;
-
 
 static void outputToStringStream(void *stream, const char *text, int len) {
   ((std::stringstream *)stream)->write(text, len);
 }
+
 
 PdfToString::PdfToString(PTSConfig config) {
   // read config file
@@ -118,92 +109,6 @@ PdfToString::~PdfToString() {
   delete globalParams;
 }
 
-// std::vector<std::string> fileToVectorString(char *fileName) {
-//   PDFDoc *doc;
-//   GString *textFileName;
-//   GString *ownerPW, *userPW;
-//   TextOutputControl textOutControl;
-//   TextOutputDev *textOut;
-//   int firstPage, lastPage;
-
-//   std::stringstream *stream = new std::stringstream();
-//   std::vector<std::string> pages;
-
-//   // read config file
-//   globalParams = new GlobalParams("");
-
-//   if (verbose) {
-//     globalParams->setPrintStatusInfo(verbose);
-//   }
-
-//   if (quiet) {
-//     globalParams->setErrQuiet(quiet);
-//   }
-
-//   // open PDF file
-//   if (ownerPassword[0] != '\001') {
-//     ownerPW = new GString(ownerPassword);
-//   } else {
-//     ownerPW = NULL;
-//   }
-//   if (userPassword[0] != '\001') {
-//     userPW = new GString(userPassword);
-//   } else {
-//     userPW = NULL;
-//   }
-
-//   doc = new PDFDoc(fileName, ownerPW, userPW);
-//   if (userPW) {
-//     delete userPW;
-//   }
-//   if (ownerPW) {
-//     delete ownerPW;
-//   }
-
-//   if (!doc->isOk()) {
-//     goto err2;
-//   }
-
-//   // construct text file name
-//   textFileName = new GString(fileName);
-
-//   firstPage = 1;
-//   lastPage = doc->getNumPages();
-
-//   textOutControl.mode = textOutTableLayout;
-//   textOutControl.clipText = clipText;
-//   textOutControl.discardDiagonalText = discardDiag;
-//   textOutControl.discardRotatedText = discardRotatedText;
-//   textOutControl.insertBOM = insertBOM;
-  
-//   textOut = new TextOutputDev(&outputToStringStream, stream, &textOutControl);
-
-//   if (textOut->isOk()) {
-//     for (int page = firstPage; page <= lastPage; page++) {
-//       stream->str("");
-//       doc->displayPages(textOut, page, page, 72, 72, 0, gFalse, gTrue, gFalse);
-//       pages.push_back(stream->str());
-//     }
-//   } else {
-//     delete textOut;
-//     goto err3;
-//   }
-//   delete textOut;
-
-//   // clean up
-//  err3:
-//   delete textFileName;
-//  err2:
-//   delete doc;
-//  err1:
-//   delete globalParams;
-
-//   // check for memory leaks
-//   Object::memCheck(stderr);
-//   gMemReport(stderr);
-
-//   return pages;
-// }
 
 int main(int argc, char **argv) {
   PTSConfig config;
