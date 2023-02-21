54e11dc3cc1b7e024eb563ddbaf8d92654b83412
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-02-21 13:09:56 +0100

    Refactor into class

--- src/xpdf-python/pdftostring.cc
+++ src/xpdf-python/pdftostring.cc
@@ -1,11 +1,3 @@
-//========================================================================
-//
-// pdftotext.cc
-//
-// Copyright 1997-2013 Glyph & Cog, LLC
-//
-//========================================================================
-
 #include <aconf.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,123 +27,64 @@
 #include "Error.h"
 #include "config.h"
 
-static int firstPage = 1;
-static int lastPage = 0;
-static double fixedPitch = 0;
-static GBool clipText = gFalse;
-static GBool discardDiag = gTrue;
-static GBool discardRotatedText = gTrue;
-static char textEncName[128] = "";
-static char textEOL[16] = "";
-static GBool noPageBreaks = gFalse;
-static GBool insertBOM = gFalse;
-static double marginLeft = 0;
-static double marginRight = 0;
-static double marginTop = 0;
-static double marginBottom = 0;
-static char ownerPassword[33] = "\001";
-static char userPassword[33] = "\001";
-static GBool verbose = gFalse;
-static GBool quiet = gFalse;
-static char cfgFileName[256] = "";
+#include "pdftostring.h"
+
+// static GBool clipText = gFalse;
+// static GBool discardDiag = gTrue;
+// static GBool discardRotatedText = gTrue;
+// static GBool noPageBreaks = gFalse;
+// static GBool insertBOM = gFalse;
+// static char ownerPassword[33] = "\001";
+// static char userPassword[33] = "\001";
+// static GBool verbose = gFalse;
+// static GBool quiet = gFalse;
 
 
 static void outputToStringStream(void *stream, const char *text, int len) {
   ((std::stringstream *)stream)->write(text, len);
 }
 
-std::vector<std::string> fileToVectorString(char *fileName) {
+PdfToString::PdfToString(PTSConfig config) {
+  // read config file
+  if (globalParams == NULL) {
+    globalParams = new GlobalParams("");
+  }
+
+  if (config.verbose) {
+    globalParams->setPrintStatusInfo(config.verbose);
+  }
+
+  if (config.quiet) {
+    globalParams->setErrQuiet(config.quiet);
+  }
+
+  textOutControl.mode = textOutTableLayout;
+  textOutControl.clipText = config.clipText;
+  textOutControl.discardDiagonalText = config.discardDiag;
+  textOutControl.discardRotatedText = config.discardRotatedText;
+  textOutControl.insertBOM = config.insertBOM;
+}
+
+std::vector<std::string> PdfToString::loadFile(char *fileName) {
   PDFDoc *doc;
   GString *textFileName;
-  GString *ownerPW, *userPW;
-  TextOutputControl textOutControl;
   TextOutputDev *textOut;
-  UnicodeMap *uMap;
-  int exitCode;
+  int firstPage, lastPage;
 
   std::stringstream *stream = new std::stringstream();
   std::vector<std::string> pages;
 
-  exitCode = 99;
+  doc = new PDFDoc(fileName);
 
-  // read config file
-  globalParams = new GlobalParams(cfgFileName);
-  if (textEncName[0]) {
-    globalParams->setTextEncoding(textEncName);
-  }
-
-  if (textEOL[0]) {
-    if (!globalParams->setTextEOL(textEOL)) {
-      fprintf(stderr, "Bad '-eol' value on command line\n");
-    }
-  }
-
-  if (noPageBreaks) {
-    globalParams->setTextPageBreaks(gFalse);
-  }
-
-  if (verbose) {
-    globalParams->setPrintStatusInfo(verbose);
-  }
-
-  if (quiet) {
-    globalParams->setErrQuiet(quiet);
-  }
-
-  // get mapping to output encoding
-  if (!(uMap = globalParams->getTextEncoding())) {
-    error(errConfig, -1, "Couldn't get text encoding");
-    goto err1;
-  }
-
-  // open PDF file
-  if (ownerPassword[0] != '\001') {
-    ownerPW = new GString(ownerPassword);
-  } else {
-    ownerPW = NULL;
-  }
-  if (userPassword[0] != '\001') {
-    userPW = new GString(userPassword);
-  } else {
-    userPW = NULL;
-  }
-  doc = new PDFDoc(fileName, ownerPW, userPW);
-  if (userPW) {
-    delete userPW;
-  }
-  if (ownerPW) {
-    delete ownerPW;
-  }
   if (!doc->isOk()) {
-    exitCode = 1;
     goto err2;
   }
-
-//   // check for copy permission
-//   if (!doc->okToCopy()) {
-//     error(errNotAllowed, -1,
-// 	  "Copying of text from this document is not allowed.");
-//     exitCode = 3;
-//     goto err2;
-//   }
 
   // construct text file name
   textFileName = new GString(fileName);
 
   firstPage = 1;
   lastPage = doc->getNumPages();
-
-  textOutControl.mode = textOutTableLayout;
-  // textOutControl.mode = textOutRawOrder;
-  textOutControl.fixedPitch = fixedPitch;
-
-  textOutControl.clipText = clipText;
-  textOutControl.discardDiagonalText = discardDiag;
-  textOutControl.insertBOM = insertBOM;
-  textOutControl.marginLeft = marginLeft;
-  textOutControl.marginRight = marginRight;
-  textOutControl.marginTop = marginTop;
-  textOutControl.marginBottom = marginBottom;
   
   textOut = new TextOutputDev(&outputToStringStream, stream, &textOutControl);
 
@@ -163,21 +96,16 @@ std::vector<std::string> fileToVectorString(char *fileName) {
     }
   } else {
     delete textOut;
-    exitCode = 2;
     goto err3;
   }
   delete textOut;
-
-  exitCode = 0;
 
   // clean up
  err3:
   delete textFileName;
  err2:
   delete doc;
-  uMap->decRefCnt();
- err1:
-  delete globalParams;
+  delete stream;
 
   // check for memory leaks
   Object::memCheck(stderr);
@@ -186,13 +114,105 @@ std::vector<std::string> fileToVectorString(char *fileName) {
   return pages;
 }
 
+PdfToString::~PdfToString() {
+  delete globalParams;
+}
+
+// std::vector<std::string> fileToVectorString(char *fileName) {
+//   PDFDoc *doc;
+//   GString *textFileName;
+//   GString *ownerPW, *userPW;
+//   TextOutputControl textOutControl;
+//   TextOutputDev *textOut;
+//   int firstPage, lastPage;
+
+//   std::stringstream *stream = new std::stringstream();
+//   std::vector<std::string> pages;
+
+//   // read config file
+//   globalParams = new GlobalParams("");
+
+//   if (verbose) {
+//     globalParams->setPrintStatusInfo(verbose);
+//   }
+
+//   if (quiet) {
+//     globalParams->setErrQuiet(quiet);
+//   }
+
+//   // open PDF file
+//   if (ownerPassword[0] != '\001') {
+//     ownerPW = new GString(ownerPassword);
+//   } else {
+//     ownerPW = NULL;
+//   }
+//   if (userPassword[0] != '\001') {
+//     userPW = new GString(userPassword);
+//   } else {
+//     userPW = NULL;
+//   }
+
+//   doc = new PDFDoc(fileName, ownerPW, userPW);
+//   if (userPW) {
+//     delete userPW;
+//   }
+//   if (ownerPW) {
+//     delete ownerPW;
+//   }
+
+//   if (!doc->isOk()) {
+//     goto err2;
+//   }
+
+//   // construct text file name
+//   textFileName = new GString(fileName);
+
+//   firstPage = 1;
+//   lastPage = doc->getNumPages();
+
+//   textOutControl.mode = textOutTableLayout;
+//   textOutControl.clipText = clipText;
+//   textOutControl.discardDiagonalText = discardDiag;
+//   textOutControl.discardRotatedText = discardRotatedText;
+//   textOutControl.insertBOM = insertBOM;
+  
+//   textOut = new TextOutputDev(&outputToStringStream, stream, &textOutControl);
+
+//   if (textOut->isOk()) {
+//     for (int page = firstPage; page <= lastPage; page++) {
+//       stream->str("");
+//       doc->displayPages(textOut, page, page, 72, 72, 0, gFalse, gTrue, gFalse);
+//       pages.push_back(stream->str());
+//     }
+//   } else {
+//     delete textOut;
+//     goto err3;
+//   }
+//   delete textOut;
+
+//   // clean up
+//  err3:
+//   delete textFileName;
+//  err2:
+//   delete doc;
+//  err1:
+//   delete globalParams;
+
+//   // check for memory leaks
+//   Object::memCheck(stderr);
+//   gMemReport(stderr);
+
+//   return pages;
+// }
+
 int main(int argc, char **argv) {
-  firstPage = 1;
-  lastPage = 1;
+  PTSConfig config;
 
   if (argc == 2) {
     int i = 0;
-    std::vector<std::string> result = fileToVectorString(argv[1]);
+    PdfToString *pts = new PdfToString(config);
+    std::vector<std::string> result = pts->loadFile(argv[1]);
+
     for (auto page : result) {
       i++;
       fprintf(stderr, "--------------------------------------- PAGE %d ---------------------------------------\n", i);
---
+++ src/xpdf-python/pdftostring.h
@@ -0,0 +1,29 @@
+#ifndef PDFTOSTRING_H
+#define PDFTOSTRING_H
+
+#include <string>
+#include <vector>
+
+#include "gtypes.h"
+#include "GlobalParams.h"
+
+typedef struct PTSConfig {
+  GBool clipText = gFalse;
+  GBool discardDiag = gTrue;
+  GBool discardRotatedText = gTrue;
+  GBool noPageBreaks = gFalse;
+  GBool insertBOM = gFalse;
+  GBool verbose = gFalse;
+  GBool quiet = gFalse;
+} PTSConfig;
+
+class PdfToString {
+public:
+    PdfToString(PTSConfig config);
+    ~PdfToString();
+    std::vector<std::string> loadFile(char *fileName);
+private:
+  TextOutputControl textOutControl;
+};
+
+#endif
