f40549e204032667ddc9b42a6d75ffe9a61eb858
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-02-23 15:14:03 +0100

    Add conversion method

---
+++ src/xpdf-python/PyCppConversion.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+#include <map>
+
+#include "Python.h"
+#include "PyCppConversion.h"
+
+using namespace std;
+
+// ------------------- Helpers for converting between Python and C++ -------------------
+
+PyObject *vectorStringToList(const vector<string> &data) {
+    PyObject *listObj = PyList_New(data.size());
+    if (!listObj) throw logic_error("Unable to allocate memory for Python list");
+
+    for (int i = 0; i < data.size(); i++) {
+        PyObject *num = PyUnicode_FromStringAndSize(data[i].c_str(), data[i].size());
+
+        if (!num) {
+            Py_DECREF(listObj);
+            throw logic_error("Unable to allocate memory for Python string");
+        }
+
+        PyList_SET_ITEM(listObj, i, num);
+    }
+
+    return listObj;
+}
---
+++ src/xpdf-python/PyCppConversion.h
@@ -0,0 +1,13 @@
+#ifndef PY_CPP_CONVERSION_H
+#define PY_CPP_CONVERSION_H
+
+#include <string>
+#include <vector>
+
+#include "Python.h"
+
+// ------------------- Helpers for converting between Python and C++ -------------------
+
+PyObject *vectorStringToList(const std::vector<std::string> &data)
+
+#endif
