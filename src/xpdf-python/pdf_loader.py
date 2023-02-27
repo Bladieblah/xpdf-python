ec6ba683466dc413e24f266887459178cd9ab246
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-02-27 15:56:44 +0100

    Fix installing, update python class

--- src/xpdf-python/pdf_loader.py
+++ src/xpdf-python/pdf_loader.py
@@ -1,26 +1,29 @@
 from typing import List
 import cXpdfPython
 
-class PdfToString:
-    def __init__(self):
-        self.pts_capsule = cXpdfPython.construct()
+class PdfLoader:
+    filename: str
 
-    def as_bytes(self, filename: str):
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.pts_capsule = cXpdfPython.construct(filename)
+
+    def extract_bytes(self):
         pages: List[bytes]
-        self.pts_capsule, pages = cXpdfPython.loadFile(self.pts_capsule, filename)
+        self.pts_capsule, pages = cXpdfPython.extractText(self.pts_capsule)
         return pages
 
-    def as_strings(self, filename: str):
-        pages: List[bytes] = self.as_bytes(filename)
+    def extract_strings(self):
+        pages: List[bytes] = self.extract_bytes()
         return [page.decode('unicode_escape', 'replace') for page in pages]
 
     def __delete__(self):
         cTokenizer.deleteObject(self.tokenizer_capsule)
 
 if __name__ == "__main__":
-    pts = PdfToString()
     path = "../NL-PDF-Extractietool/data/input/9999748983.pdf"
-    result = pts.as_strings(path)
+    loader = PdfLoader(path)
+    result = loader.extract_strings()
     print(len(result))
 
     for i, page in enumerate(result):
