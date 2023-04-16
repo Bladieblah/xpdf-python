e16c5000fac91d9084b6d5dce7838ef004dc7cbe
ReMiOS <r.e.oort@concepts.nl>
2023-04-16 05:08:00 +0200

    Update PdfLoader.h

--- src/xpydf/PdfLoader.h
+++ src/xpydf/PdfLoader.h
@@ -35,7 +35,7 @@ public:
     std::vector<std::string> extractText();
     std::vector<PageImageInfo> extractImages();
     bool isOk();
-	  int getErrorCode();
+    int getErrorCode();
 private:
   TextOutputControl textOutControl;
   PDFDoc *doc;
