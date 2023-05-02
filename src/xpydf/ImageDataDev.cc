ee4bdd1c611f361e8c09ccc8456d563a7ab4edc7
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-05-02 14:54:37 +0200

    Read grayscale images as well

--- src/xpydf/ImageDataDev.cc
+++ src/xpydf/ImageDataDev.cc
@@ -24,43 +24,78 @@ void ImageDataDev::drawImage(GfxState *state, Object *ref, Stream *str,
   int *maskColors, GBool inlineImg,
   GBool interpolate
 ) {
+  GfxColorSpaceMode csMode;
   ImageStream *imgStr;
   Guchar *p;
   GfxRGB rgb;
+  GfxGray gray;
+  Image image;
   int x, y;
   int n = 0;
-  
-  Image image = {
-    static_cast<unsigned int>(IMAGE_TYPES::P6),
-    static_cast<unsigned int>(width),
-    static_cast<unsigned int>(height),
-    static_cast<unsigned int>(3 * width * height),
-    nullptr
-  };
-  image.data = (unsigned char *)malloc(image.size * sizeof(unsigned char));
-  imgNum++;
+
+  csMode = colorMap->getColorSpace()->getMode();
+  if (csMode == csIndexed) {
+    csMode = ((GfxIndexedColorSpace *)colorMap->getColorSpace())
+             ->getBase()->getMode();
+  }
 
   // initialize stream
   imgStr = new ImageStream(str, width, colorMap->getNumPixelComps(), colorMap->getBits());
   imgStr->reset();
+  imgNum++;
 
-  for (y = 0; y < height; ++y) {
-    if ((p = imgStr->getLine())) {
-      for (x = 0; x < width; ++x) {
-        colorMap->getRGB(p, &rgb, state->getRenderingIntent());
-        image.data[n++] = colToByte(rgb.r);
-        image.data[n++] = colToByte(rgb.g);
-        image.data[n++] = colToByte(rgb.b);
-        p += colorMap->getNumPixelComps();
+  if (colorMap->getNumPixelComps() == 1 && (csMode == csDeviceGray || csMode == csCalGray)) {
+    image = {
+      static_cast<unsigned int>(IMAGE_TYPES::P5),
+      static_cast<unsigned int>(width),
+      static_cast<unsigned int>(height),
+      static_cast<unsigned int>(width * height),
+      nullptr
+    };
+    image.data = (unsigned char *)malloc(image.size * sizeof(unsigned char));
+
+    for (y = 0; y < height; ++y) {
+      if ((p = imgStr->getLine())) {
+        for (x = 0; x < width; ++x) {
+          colorMap->getGray(p, &gray, state->getRenderingIntent());
+          image.data[n++] = colToByte(gray);
+          ++p;
+        }
+      } else {
+        for (x = 0; x < width; ++x) {
+          image.data[n++] = 0;
+        }
       }
-    } else {
-      for (x = 0; x < width; ++x) {
-        image.data[n++] = 0;
-        image.data[n++] = 0;
-        image.data[n++] = 0;
+    }
+  } else {
+    image = {
+      static_cast<unsigned int>(IMAGE_TYPES::P6),
+      static_cast<unsigned int>(width),
+      static_cast<unsigned int>(height),
+      static_cast<unsigned int>(3 * width * height),
+      nullptr
+    };
+    image.data = (unsigned char *)malloc(image.size * sizeof(unsigned char));
+
+    for (y = 0; y < height; ++y) {
+      if ((p = imgStr->getLine())) {
+        for (x = 0; x < width; ++x) {
+          colorMap->getRGB(p, &rgb, state->getRenderingIntent());
+          image.data[n++] = colToByte(rgb.r);
+          image.data[n++] = colToByte(rgb.g);
+          image.data[n++] = colToByte(rgb.b);
+          p += colorMap->getNumPixelComps();
+        }
+      } else {
+        for (x = 0; x < width; ++x) {
+          image.data[n++] = 0;
+          image.data[n++] = 0;
+          image.data[n++] = 0;
+        }
       }
     }
   }
+
 
   imgStr->close();
   delete imgStr;
